036ecb4ac58df047722d8cff8352f86d4dad3e3d
dbabusci <98336934+dbabusci@users.noreply.github.com>
2024-04-20 16:35:16 -0400

    Checkout function mostly good

--- HareSVN/src-tauri/src/main.rs
+++ HareSVN/src-tauri/src/main.rs
@@ -1,24 +1,74 @@
+use std::process::Command;
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    return format!("Hello, {}! You've been greeted from Rust!", name);
+fn greet(name: &str) -> String{
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", "mkdir ~/test"])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("mkdir ~/Documents/SVN")
+            .output()
+            .expect("failed to execute process")
+    };
+    
+    let hello = output.stdout;
+    return hello.into_iter().map(|x| x as char).collect::<String>();
 }
 
+/*
+Redesign so it is not stupid
+May not have to have a return value
+*/
 #[tauri::command]
-fn checkout(repo_name: String) {
-    if !std::path::Path::new("Documents/SVN/").exists() {
-        let _ = std::process::Command::new("cd");
-        let _ = std::process::Command::new("cd Documents");
-        let _ = std::process::Command::new("mkdir SVN");
-    }
-    let _ = std::process::Command::new("cd");
-    let _ = std::process::Command::new("cd Documents/SVN");
-    let _ = std::process::Command::new("svn checkout")
-        .arg(repo_name);
-        
+fn checkout(name: String) -> String {
+    if cfg!(target_os = "windows") {
+        if !std::path::Path::new("~/Documents/SVN").exists(){
+            let output = Command::new("cmd")
+                .args(["/C", "mkdir ~/test"])
+                .output()
+                .expect("failed to execute process");
+        }
+        let output = Command::new("cmd")
+            .args(["/C", "cd ~/Documents/SVN"])
+            .output()
+            .expect("Changing dir to SVN");
+        let output = Command::new("cmd")
+            .args(["/C", "svn checkout", &name])
+            .output()
+            .expect("Repo failed to chekc out");
+    } 
+    else {
+        if !std::path::Path::new("~/Documents/SVN").exists(){
+            let output = Command::new("sh")
+                .arg("-c")
+                .arg("mkdir ~/Documents/SVN")
+                .output()
+                .expect("failed to execute process");
+        }
+        let output = Command::new("sh")
+            .args(["-c", "cd ~/Documents/SVN"])
+            .output()
+            .expect("Changing dir to SVN");
+        let output = Command::new("sh")
+            .args(["-c", "svn checkout", &name])
+            .output()
+            .expect("Repo failed to chekc out");
+    };
+    let output = Command::new("sh")
+        .args(["echo", "Shut up"])
+        .output()
+        .expect("Shut up nerd I hate rust");
+    
+    let hello = output.stdout;
+    return hello.into_iter().map(|x| x as char).collect::<String>();
 }
 
 #[tauri::command]
@@ -40,7 +90,7 @@ fn commit(selected_files: std::vec::Vec<String>) {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, checkout])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
