cff3a560c3c87cca40ce3754c83aebbd8215b2bd
dbabusci <98336934+dbabusci@users.noreply.github.com>
2024-04-20 13:35:47 -0400

    WIP

--- HareSVN/src-tauri/src/main.rs
+++ HareSVN/src-tauri/src/main.rs
@@ -7,6 +7,28 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn get_files(repo: String) -> std::Vec<String> {
+    
+}
+
+#[tauri::command]
+fn checkout(folder_name: String) {
+    std::fs::create_dir(folder_name)?;
+}
+
+#[tauri::command]
+fn commit(selected_files: std::Vec<String>) {
+    let mut file_string: String = String::new();
+    for file in selected_files {
+        file_string = " " + file;
+    }
+    let commit_command: std::process::Command = std::process::Command::new("svn")
+        .arg(&file_string)
+        .output()
+        .map_err(|e|, format!("Failed to execute command {}", e))?;
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
