c97f6ad3855339ccf4bd40cbe6b0276202765c5b
whelblin <whelblin@kent.edu>
2024-04-20 21:35:52 -0400

    adding my studd

--- HareSVN/src/components/MainScreen.vue
+++ HareSVN/src/components/MainScreen.vue
@@ -1,8 +1,33 @@
 <script setup lang="ts">
-const props = defineProps(['repo'])
+const props = defineProps(['repo', 'files'])
+import { invoke } from '@tauri-apps/api/tauri'
+
+
 </script>
 
 <template>
-    <div class="flex">{{ props.repo }}</div>
+    <ul class="block w-full bg-gray-50">
+        <div class="grid grid-cols-2 pb-4">
+            <div class="text-center font-bold">
+                <h1>Status</h1>
+            </div>
+            <div class="text-center font-bold">
+                <h1>File Name</h1>
+            </div>
+        </div>
+        <hr class="border-1 pt-1">
+        <li v-for="item in props.files" :key="item.fileName" class="py-2">
+            <div class="grid grid-cols-2">
+                <div class="text-center">
+                    {{ item.status }}
+                </div>
+                <div class="text-center">
+                    {{ item.fileName }}
+                </div>
+            </div>
+            <hr class="border-1">
+        </li>
+    </ul>
 
 </template>
+
--- HareSVN/src/components/SideNav.vue
+++ HareSVN/src/components/SideNav.vue
@@ -1,22 +1,43 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { invoke } from '@tauri-apps/api/tauri'
-const temp = ref(["one", "two", "three"])
+const temp = ref([{url:"http:localhost/repo", name: "repo"}])
 const input = ref({
   url: "",
   name: "",
 })
-const result = ref("")
+const checkout = ref("")
+const result = defineModel("files", {default: [{status:"", fileName:""}]})
 const active = defineModel("repo")
 async function addRepo(e: { preventDefault: () => void; }){
   e.preventDefault()
   input.value.name = input.value.url.substring(input.value.url.lastIndexOf("/") + 1, input.value.url.length)
-  temp.value.push(input.value.name)
-  result.value = await invoke("checkout", { url: input.value.url, name: input.value.name });
+  temp.value.push(input.value)
+  checkout.value = await invoke("checkout", { url: input.value.url, name: input.value.name });
   input.value = {url: "", name:""}
 }
-function changeActive(item:string){
+async function changeActive(item:string){
   active.value = item
+  result.value = []
+   let temp:Array<string>= await invoke("status", { name:item });
+   temp.map((file:string)=>{
+      let temp= file.split(" ").filter((character)=> character != "")
+      if (temp[0].charAt(temp[0].length-1) == "?" && temp[temp.length-1] != "."){
+        result.value.push({status: "Untracked", fileName: temp[temp.length-1]})
+      }
+      else if (temp[0].charAt(temp[0].length-1) == "A" && temp[temp.length-1] != "."){
+        result.value.push({status: "Added", fileName: temp[temp.length-1]})
+      }
+      else if (temp[0].charAt(temp[0].length-1) == "M" && temp[temp.length-1] != "."){
+        result.value.push({status: "Modified", fileName: temp[temp.length-1]})
+      }
+      else if(temp[temp.length-1] != "." && temp[temp.length-1] != ""){
+        result.value.push({status: "Up To Date", fileName: temp[temp.length-1]})
+      }
+      return file.split(" ").filter((character)=> character != "")
+    })
+    console.log(result.value)
+
   
 }
 
@@ -31,18 +52,18 @@ function setItemCSS(item:string){
 </script>
 
 <template>
-    <div class="min-h-screen w-[20%] bg-gray-100">
+    <div class="min-h-screen min-w-[20%] bg-gray-100 border-r-2">
       <form v-on:submit="addRepo">
         <div class="flex mt-2 text-center bg-gray-200">
-          <label for="email" class="block font-medium text-sm text-gray text-center">Enter Repository URL</label>
-          <div class=" flex mt-1 my-2 px-2 justify-center">
-            <input id="repo" name="repo" v-model = "input.url" type="text" required class="block w-full bg-gray-100 rounded-md py-1 pl-2 mt-2 ring-1 ring-gray-400 shadow-md {{ if active == item ? 'text-blue-100' :'text-red-400'}}">
+          <label for="repo" class="block font-bold text-sm text-center">Enter Repository URL</label>
+          <div class="flex mt-1 my-2 px-2 justify-center">
+            <input id="repo" name="repo" v-model = "input.url" type="text" required class="block w-full bg-gray-100 rounded-md pl-1 m-1 shadow-md">
           </div>
         </div>
       </form>
       <hr>
       <div>
-        <div v-for="item in temp" :key="item" @click="changeActive(item)" :class="setItemCSS(item)">{{item}}</div>
+        <div v-for="item in temp" :key="item.name" @click="changeActive(item.name)" :class="setItemCSS(item.name)">{{item.name}}</div>
       </div>
     </div>
 
